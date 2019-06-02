4e46b93db216b0f2f31a05c7b1ade54d9e68da89
rathakarn <gkrgroup.co.th@gmail.com>
2019-06-02 17:39:55 +0700

    animated sucess

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'screens/home.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -13,4 +15,19 @@ class MyApp extends StatelessWidget {
       home: Home(),
     );
   }
+}
+
+class Home extends StatelessWidget{
+@override
+ Widget build(BuildContext context){
+return Scaffold(
+backgroundColor: Colors.black,
+body: Center(
+  child: SpinKitCircle(color: Colors.lightBlueAccent, size: 200.0,),
+),
+
+);
+
+ } 
+
 }
